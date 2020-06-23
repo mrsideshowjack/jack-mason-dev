0aa85ef0ff6ad82ae0d22fde0bb202d41d358889
Jack Mason <jack.mason1@ibm.com>
2020-06-23 11:14:35 +0100

    feat: move about to component

---
+++ src/components/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="about">
+    <h2>h2. About me</h2>
+    <p>
+      Hi, I’m Jack and I am full stack web developer currently working at IBM in London as part of the <a href="https://ibm-cio-london.github.io/studio-hiring/">CIO sub domain</a> working on <a href="https://www.ibm.com/partnerworld/public">PartnerWorld</a>; This is IBM’s largest microsite with tens of thousands of pages and users, with mixes of legacy and brand new code bases.
+    </p>
+
+    <p>
+      I get to work on lots of exciting projects, and I am always seeking new skills and improving my existing ones. We make time in our sprints to spend time experimenting with new technologies, contributing to open source as well as going to tech meetups across London. 
+    </p>
+
+    <p>
+      I graduated from The University of Portsmouth in 2018, with a Bachelor’s of Science Degree in <a href="http://psd.docstore.port.ac.uk/C2187S.pdf">Web Technologies.</a>
+    </p>
+
+    <p>
+      Previously I was a full stack developer with <a href="https://kn-i.com">Knownow Information ltd</a> for almost 3 years, where I got work on projects such as <a href="https://consentua.com/">Consentua</a> and <a href="https://entelligently.com/">entelligently.</a>
+    </p>
+    <p>Here are some of the technologies I am experienced with:</p>
+    <ul>
+      <li>HTML5, CSS, JS, ES6 etc</li>
+      <li>SPA’s with VueJS, React, Polymer</li>
+      <li>Git, CI Services,  Gulp & webpack</li>
+      <li>REST & graphQL</li>
+      <li>Backends with Node & server-less functions</li>
+      <li>Databases from DB2 to MongoDB</li>
+      <li>IBM Cloud, Google Cloud Platform & Azure</li>
+      <li>Sketch, Figma + Photoshop</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+    name: 'About',
+}
+</script>
+<style lang="css">
+
+</style>
